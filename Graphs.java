9abedf8bf12e0c736614ce878d9607f6e039cbbb
hiiyasu <108071643+hiiyasu@users.noreply.github.com>
2022-11-15 13:21:50 -0500

    Add files via upload

---
+++ Graphs.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Graphs {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+        while(choice != 8){
+            System.out.println("1. Is Connected\n" +
+                        "2. Minimum Spanning Tree\n" +
+                        "3. Shortest Path\n" +
+                        "4. Is Metric\n" +
+                        "5. Make Metric\n" +
+                        "6. Traveling Salesman Problem\n" +
+                        "7. Approximate TSP\n" +
+                        "8. Quit\n" +
+                        "Make your choice (1-8): ");
+            choice = in.nextInt();
+            if(choice < 0 || choice > 8) {
+                System.out.println("Please choose a valid option. \n");
+            }
+        }
+    }
+}
