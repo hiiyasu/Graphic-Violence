f76986c66ab534b9d3915568cf83adf2370b3c43
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2022-11-28 15:11:41 -0500

    Update Graph.java
    Added stubs and implemented isConnected

--- Graph.java
+++ Graph.java
@@ -76,6 +76,33 @@ public class Graph {
     public void addEdge(int v, int w, int weight){
         adj[v].put(w, weight);
     }
+    
+    public boolean isConnected() {
+		if (isConnected != null) return isConnected;
+		DepthFirstSearch dfs = new DepthFirstSearch(this, 0);
+		boolean isConnected = true;
+		for (int i = 0; i < adj.length && isConnected; i++) {
+			if (!dfs.marked(i)) isConnected = false;
+		}
+		this.isConnected = isConnected;
+		return isConnected;
+	}
+
+	public String mst() {
+		return "";
+	}
+
+	public String shortestPath(int v) {
+		return "";
+	}
+
+	public boolean isMetric() {
+		return false;
+	}
+
+	public String makeMetric() {
+		return "";
+	}
 }
 
 
